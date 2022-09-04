e394602fd9ff99c35f44c3ec79cc4d13e87f2305
paarvi344 <78478724+paarvi344@users.noreply.github.com>
2022-09-04 22:31:28 +0530

    Add files via upload

---
+++ webscraping.py
@@ -0,0 +1,37 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+import time 
+import csv 
+START_URL = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
+browser = webdriver.Chrome("C:\Users\Paarvi\Desktop\web scraping\chromedriver.exe")
+browser.get(START_URL)
+time.sleep(10)
+def scrape():
+    headers = ["star_name", "Distance", "Mass", "Radius"]
+    star_data = []
+    for i in range(0,428):
+        soup = BeautifulSoup(browser.page_source,"html.parcel")
+        for ul_tag in soup.find_all("ul",attrs = {"class","exoplanet"}):
+            li_tag = ul_tag.find_all("li")
+            temp_list = []
+            for index , li_tag in enumerate(li_tag):
+                if index == 0 :
+                    temp_list.append(li_tag.find_all("a"[0].contents[0]))
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            star_data.append(temp_list)
+            browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+        with open("scraper_2.csv","w") as f :
+            csvwriter = csv.writer(f)
+            csvwriter.writerow(headers)
+            csvwriter.writerows(star_data)
+scrape()
+
+
+
+
